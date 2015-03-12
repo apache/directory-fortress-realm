795b3d0b62f033089c40c775029fce50e9f24f68
Shawn McKinney <smckinney@apache.org>
2015-03-12 16:55:09 -0500

    FC-79 - add common error ids to realm

---
+++ impl/src/main/java/org/apache/directory/fortress/realm/GlobalIds.java
@@ -0,0 +1,13 @@
+package org.apache.directory.fortress.realm;
+
+/**
+ * Created by smckinn on 3/12/15.
+ */
+public class GlobalIds
+{
+    public static int CONTEXT_SERIALIZATION_FAILED = 201;
+    public static int CONTEXT_DESERIALIZATION_FAILED_IO = 202;
+    public static int CONTEXT_DESERIALIZATION_FAILED_UNSUPPORTED_ENCODING = 203;
+    public static int CONTEXT_DESERIALIZATION_FAILED_CLASS_NOT_FOUND = 204;
+    public static int SESSION_INITIALIZATION_FAILED = 205;
+}
--- impl/src/main/java/org/apache/directory/fortress/realm/J2eePolicyMgrImpl.java
+++ impl/src/main/java/org/apache/directory/fortress/realm/J2eePolicyMgrImpl.java
@@ -59,10 +59,6 @@ public class J2eePolicyMgrImpl implements J2eePolicyMgr
     private static AccessMgr accessMgr;
     private static ReviewMgr reviewMgr;
     private static final String SESSION = "session";
-    private static int CONTEXT_SERIALIZATION_FAILED = 201;
-    private static int CONTEXT_DESERIALIZATION_FAILED_IO = 202;
-    private static int CONTEXT_DESERIALIZATION_FAILED_UNSUPPORTED_ENCODING = 203;
-    private static int CONTEXT_DESERIALIZATION_FAILED_CLASS_NOT_FOUND = 204;
 
     static
     {
@@ -558,7 +554,7 @@ public class J2eePolicyMgrImpl implements J2eePolicyMgr
             catch ( IOException ioe )
             {
                 String error = "serialize caught IOException: " + ioe;
-                throw new SecurityException(CONTEXT_SERIALIZATION_FAILED, error);
+                throw new SecurityException( org.apache.directory.fortress.realm.GlobalIds.CONTEXT_SERIALIZATION_FAILED, error);
             }
         }
         
@@ -585,17 +581,17 @@ public class J2eePolicyMgrImpl implements J2eePolicyMgr
         }
         catch (java.io.UnsupportedEncodingException e)
         {
-            throw new SecurityException( CONTEXT_DESERIALIZATION_FAILED_UNSUPPORTED_ENCODING, "deserialize caught UnsupportedEncodingException:" + e);
+            throw new SecurityException( org.apache.directory.fortress.realm.GlobalIds.CONTEXT_DESERIALIZATION_FAILED_UNSUPPORTED_ENCODING, "deserialize caught UnsupportedEncodingException:" + e);
         }
         catch (IOException e)
         {
             LOG.warn( "deserialize caught IOException:" + e);
-            throw new SecurityException( CONTEXT_DESERIALIZATION_FAILED_IO, "deserialize caught IOException:" + e);
+            throw new SecurityException( org.apache.directory.fortress.realm.GlobalIds.CONTEXT_DESERIALIZATION_FAILED_IO, "deserialize caught IOException:" + e);
         }
         catch (ClassNotFoundException e)
         {
             LOG.warn( "deserialize caught ClassNotFoundException:" + e);
-            throw new SecurityException( CONTEXT_DESERIALIZATION_FAILED_CLASS_NOT_FOUND, "deserialize caught ClassNotFoundException:" + e);
+            throw new SecurityException( org.apache.directory.fortress.realm.GlobalIds.CONTEXT_DESERIALIZATION_FAILED_CLASS_NOT_FOUND, "deserialize caught ClassNotFoundException:" + e);
         }
     }
 }
